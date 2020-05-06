0005ff4f19407e5ecaf673f1a308edd2edfb546e
Giovanni Vargas <jonan-vargas23@hotmail.com>
2020-05-05 20:06:29 -0500

    Se comsumio el servcio de moviesPopular en el servicio movies.service.ts y se pinto el componente app.component.ts con el httpClient

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+import { MoviesService } from './services/movies.service';
+import { first } from 'rxjs/operators';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +10,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'movies-angularjs';
+
+  constructor(
+    private moviesService: MoviesService
+  ) {
+    this.getPopular();
+  }
+
+  getPopular() {
+    this.moviesService.getPopular()
+      .pipe(first())
+      .subscribe(response => {
+        console.log(response);
+      });
+  }
 }
--- src/app/services/movies.service.ts
+++ src/app/services/movies.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
 
+import { HttpClient, JsonpInterceptor } from '@angular/common/http';
+import { map } from "rxjs/operators";
+
 @Injectable({
   providedIn: 'root'
 })
 export class MoviesService {
 
-  constructor() { }
+  private apikey: string = "eeba7a481f04ca04446e13ca95af978d";
+  private urlMoviedb: string = "https://api.themoviedb.org/3/";
+
+  constructor(
+    private http: HttpClient,
+    private jsonp: JsonpInterceptor
+  ) { }
+
+  private getQuery() {
+    const url = `${ this.urlMoviedb }movie/popular?api_key=${ this.apikey }&language=es-CO&page=1`; //&callback=test
+
+    return this.http.get(url);
+  }
+
+  getPopular() {
+    return this.getQuery()
+      .pipe(map(response => {
+        return response;
+      }));
+  }
 }
