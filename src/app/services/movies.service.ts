00e5d3f742df576cbe14e6800e7f32de2432e5ba
Giovanni Vargas <jonan-vargas23@hotmail.com>
2020-05-11 19:36:56 -0500

    Se corrigio agrego backgroud-position:center al cover de las div-1 y div-2, se corrigio error en fechas de busqueda y se hizo metodo de getDate() y getBilboardChildren()

--- src/app/services/movies.service.ts
+++ src/app/services/movies.service.ts
@@ -10,42 +10,63 @@ export class MoviesService {
 
   private apikey: string = "eeba7a481f04ca04446e13ca95af978d";
   private urlMoviedb: string = "https://api.themoviedb.org/3/";
+  private language: string = "es-CO";
 
   constructor(
     private http: HttpClient,
   ) { }
 
-  private getQuery() {
-    const url = `${ this.urlMoviedb }movie/popular?api_key=${ this.apikey }&language=es-CO&page=1`;
-
+  private getQuery(query: string) {
+    const url = `${ this.urlMoviedb }discover/movie?api_key=${ this.apikey }&language=${ this.language }&page=1${ query }`;
     return this.http.jsonp(url, 'callback=test');
   }
 
   getPopular() {
-    return this.getQuery()
+    let query = `&sort_by=popularity.desc&include_adult=false&include_video=false`;
+    return this.getQuery(query)
       .pipe(map(response => {
         return response['results'];
       }));
   }
+
+  getPopularClildren() {
+    let query = `&sort_by=popularity.desc&include_adult=false&include_video=false&with_genres=12%2C16%2C35%2C10751`;
+    return this.getQuery(query)
+      .pipe(map(response => {
+        return response['results'];
+      }));
+  }
+
 
   getBillboard() {
     let dateFrom = new Date();
     let dateTo = new Date();
     dateTo.setDate(dateTo.getDate() + 7);
 
-    let dateFromStr = `${ dateFrom.getFullYear() }-0${ dateFrom.getMonth() + 1 }-0${ dateFrom.getDay() }`;
-    let dateToStr = `${ dateTo.getFullYear() }-0${ dateTo.getMonth() + 1 }-0${ dateTo.getDay() }`;
-    console.log(dateFromStr);
+    let dateFromStr = this.getDate(dateFrom);
+    let dateToStr = this.getDate(dateTo);
 
-    const url = `${ this.urlMoviedb }discover/movie?api_key=${ this.apikey }&language=es-CO&sort_by=popularity.desc&page=1&release_date.gte=${ dateFromStr }&release_date.lte=${ dateToStr }`;
-    return this.http.jsonp(url, 'callback=test')
+    let query = `&sort_by=popularity.desc&release_date.gte=${ dateFromStr }&release_date.lte=${ dateToStr }`;
+    return this.getQuery(query)
       .pipe(map(response => {
           return response['results'];
         }));
-    // return this.getQuery()
-    //   .pipe(map(response => {
-    //     return response;
-    //   }));
-    // TODO: Hacer que el servicio sea general con un solo getQuery
+  }
+
+  private getDate(date: Date): string {
+    let year = date.getFullYear().toString();
+    let month = (date.getMonth() + 1).toString();
+    let day = date.getDate().toString();
+
+    if(month.length < 2) {
+      month = '0' + month;
+    }
+
+    if(day.length < 2) {
+      day = '0' + day;
+    }
+
+    let newDate = year + '-' + month + '-' + day;
+    return newDate;
   }
 }
