9f53fd02713a84d535665e019a80f1c007db071e
Johanny Vargas González <jonan-vargas23@hotmail.com>
2021-04-03 21:08:00 -0500

    Se hace redireccion con el router y se envia parametros cuando se pulsa sobre la pelicula del componente moviePosterGrid

--- src/app/components/movies-poster-grid/movies-poster-grid.component.ts
+++ src/app/components/movies-poster-grid/movies-poster-grid.component.ts
@@ -1,5 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
+
+// Imports
 import { Bilboard } from '../../interfaces/movie.interface';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-movies-poster-grid',
@@ -11,9 +14,16 @@ export class MoviesPosterGridComponent implements OnInit {
 
   @Input() public movies: Bilboard[];
 
-  constructor() { }
+  constructor(
+    private router: Router,
+
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  public onMovieClick(id: number) {
+    this.router.navigate(['/movie', id, 'home']);
   }
 
 }
